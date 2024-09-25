387ecae676cfc1d3a942fb280fd60b1bc3e032ec
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2024-09-25 16:56:14 +0300

    Design Course Details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CourseDetailsPage from './pages/CourseDetailsPage';
 function App() {
   return (
     <div className="App">
-      
+      <CourseDetailsPage />
     </div>
   );
 }
