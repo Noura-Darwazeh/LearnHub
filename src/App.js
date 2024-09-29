d286e9dc78af764f09dbb0bd8671092000894a3b
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2024-09-29 17:43:05 +0300

    fetch courses from api for home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import CoursesPage from  './pages/CoursesPage'
+
 function App() {
   return (
     <div className="App">
-      <CoursesPage/>
+
     </div>
   );
 }
